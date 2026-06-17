1e490e76098ea4b47e64551a32104e92c398e40d
jaereeeee <jerelineannrosa@gmail.com>
2026-06-17 14:30:53 +0800

    ykykyky

--- Basic_Structure_and_Syntax/Main.java
+++ Basic_Structure_and_Syntax/Main.java
@@ -7,10 +7,18 @@ public class Main {
         System.out.println(223501140856l);
         System.out.println("I'm 17 years old");
         System.out.println("I live in Caloocan City");
-        System.out.println("Addition: 5 + 3 = " + (5 + 3));
-        System.out.println("Subtraction: 5 - 3 = " + (5 - 3));
-        System.out.println("Multiplication: 5 * 3 = " + (5 * 3));
-        System.out.println("Division: 5 / 3 = " + (5 / 3));
+        System.out.println("Addition");
+        System.out.print("5 + 3 = ");
+        System.out.println(5 + 3);
+        System.out.println("Subtraction");
+        System.out.print("5 - 3 = ");
+        System.out.println(5 - 3);
+        System.out.println("Multiplication");
+        System.out.print("5 * 3 = ");   
+        System.out.println(5 * 3);
+        System.out.println("Division"); 
+        System.out.print("5 / 3 = ");
+        System.out.println(5 / 3);
         System.out.println("I am a student of CZSHS and currently in Grade 12 section Peridot");
     }
 }
