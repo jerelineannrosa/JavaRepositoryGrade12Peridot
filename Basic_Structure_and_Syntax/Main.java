dabb357dd3a191a8b1fc6f004fe27d98709e659d
jaereeeee <jerelineannrosa@gmail.com>
2026-06-17 14:42:38 +0800

    06-17-26

--- Basic_Structure_and_Syntax/Main.java
+++ Basic_Structure_and_Syntax/Main.java
@@ -1,27 +1,9 @@
 package Basic_Structure_and_Syntax;
 
 public class Main {
-        public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("I'm Jereline Ann L. Rosa");
-        System.out.println(223501140856l);
-        System.out.println("I'm 17 years old");
-        System.out.println("I live in Caloocan City");
-        System.out.println("Addition");//This is addition
-        System.out.print("5 + 3 = ");
-        System.out.println(5 + 3);
-        System.out.println("Subtraction");//This is subtraction
-        System.out.print("5 - 3 = ");
-        System.out.println(5 - 3);
-        System.out.println("Multiplication");//This is multiplication
-        System.out.print("5 * 3 = ");   
-        System.out.println(5 * 3);
-        System.out.println("Division");//This is division
-        System.out.print("5 / 3 = ");
-        System.out.println(5 / 3);
-        /*This is a multi-line comment
-        that will show how to use multi-line comments in Java
-        within this comment, you can write multiple lines of text
-        withouth having to use // for each line */
-    }
+    public static void main(String[] args) {
+
+
+}
+
 }
