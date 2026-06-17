5a2d7b9af9d31835fc1a56cb17dd352ef16fee50
jaereeeee <jerelineannrosa@gmail.com>
2026-06-17 14:17:52 +0800

    wkwkw

--- Basic_Structure_and_Syntax/Main.java
+++ Basic_Structure_and_Syntax/Main.java
@@ -4,10 +4,17 @@ public class Main {
         public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("I'm Jereline Ann L. Rosa");
+        System.out.println(223501140856l);
         System.out.println("I'm 17 years old");
         System.out.println("I live in Caloocan City");
-        System.out.println("My favorite color is pink");
-        System.out.println("My hobbies are playing video games, watching movies, and listening to music");
+        System.out.println("Addition");
+        System.out.println(5 + 3);
+        System.out.println("Subtraction");
+        System.out.println(5 - 3);
+        System.out.println("Multiplication");   
+        System.out.println(5 * 3);
+        System.out.println("Division");
+        System.out.println(5 / 3);
         System.out.println("I am a student of CZSHS and currently in Grade 12 section Peridot");
     }
 }
