7acc1e4043fd28ccdac345909b0edb771568c7c2
jaereeeee <jerelineannrosa@gmail.com>
2026-06-19 13:59:50 +0800

    06-19-26 commited

---
+++ Basic_Structure_and_Syntax/Datatype.java
@@ -0,0 +1,33 @@
+package Basic_Structure_and_Syntax;
+
+public class Datatype {
+    public static void main(String[] args) {
+       System.out.println("Activity # 2 - Java Datatypes");
+       byte myByte = 127;
+       System.out.println("Byte Value: " + myByte);
+       short myShort = 18674;
+       System.out.println("Short Value: " + myShort);
+       int myInt = 276384936;
+       System.out.println("Integer Value: " + myInt);
+       long myLong = 1234567890123l;
+       System.out.println("Long Value: " + myLong);
+       float myFloat = 9.67f;
+       System.out.println("Float Value: " + myFloat);
+       double myDouble = 25.65d;
+       System.out.println("Double Value: " + myDouble);
+       float myScientificFloat = 3.58e3f; //3.58 x 10^3
+       System.out.println("Scientific Notation Float Value: " + myScientificFloat);
+       double myScientificDouble = 4.56e-2d; //4.56 x 10^-2
+       System.out.println("Scientific Notation Double Value: " + myScientificDouble);
+       boolean myBoolean = true;
+       System.out.println("Boolean Value: " + myBoolean);
+       char myChar = 'A';
+       System.out.println("Character Value: " + myChar);
+       char myVar1 = 123, myVar2 = 125, myVar3 = 121;
+       System.out.println(myVar1);
+       System.out.println(myVar2);
+       System.out.println(myVar3);
+       String myNull = null;
+       System.out.println("Null value: " + myNull);
+    }
+}
---
+++ Basic_Structure_and_Syntax/javadatatype.java
@@ -0,0 +1,32 @@
+package Basic_Structure_and_Syntax;
+
+public class javadatatype {
+    public static void main(String[] args) {
+        System.out.println("Java Data Type Activity");
+        byte playerlevel = 12;
+        System.out.println("Player Level: " + playerlevel);
+        short playerMaxHp = 5000;
+        System.out.println("Player Max HP: " + playerMaxHp);
+        int gameScore = 145820;
+        System.out.println("Game Score: " + gameScore);
+        long gamePlayTimeMs = 86400000l;
+        System.out.println("Game Play Time (ms): " + gamePlayTimeMs);
+        float playerSpeed = 5.75f;
+        System.out.println("Player Speed: " + playerSpeed);
+        double playerExactXCoordinate = 142.8594023d;
+        System.out.println("Player Exact X Coordinate: " + playerExactXCoordinate);
+        double playerExactYCoordinate = 98.4567890123d;
+        System.out.println("Player Exact Y Coordinate: " + playerExactYCoordinate);
+        float playerHealthPercentage = 75.5f;
+        System.out.println("Player Health Percentage: " + playerHealthPercentage);
+        boolean isPlayerAlive = true;
+        System.out.println("Is Player Alive: " + isPlayerAlive);
+        char playerRank = 'A';
+        System.out.println("Player Rank: " + playerRank);   
+        char playerVar1 = 118, playerVar2 = 120, playerVar3 = 117;
+        System.out.println("Player Var1: " + playerVar1);
+        System.out.println("Player Var2: " + playerVar2);
+        System.out.println("Player Var3: " + playerVar3);
+
+    }
+}
