fc70b0e498a98d34d44e906f7223f5e0ffee8b87
jaereeeee <jerelineannrosa@gmail.com>
2026-07-07 14:40:36 +0800

    YKYKY

--- Operations/incrementactivity.java
+++ Operations/incrementactivity.java
@@ -2,10 +2,11 @@ package Operations;
 
 public class incrementactivity {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 67;
+        int x = 12;
+        int y = 25;
         System.out.println("Product of: ");
         System.out.println(x * y);
+
         System.out.println(++x / --y);
         
     }
