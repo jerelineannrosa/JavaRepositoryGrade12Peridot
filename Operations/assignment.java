f6227954698ebc3b9e77e1267ef4e01edfb5a9d4
jaereeeee <jerelineannrosa@gmail.com>
2026-07-09 14:50:25 +0800

    wwkkk

---
+++ Operations/assignment.java
@@ -0,0 +1,11 @@
+package Operations;
+
+public class assignment {
+    public static void main(String[] args) {
+        int a = 3, b = -2;
+        System.out.println(b %= a);
+        System.out.println(b *= a++);
+        System.out.println(a /= --b);
+
+    }
+}
--- Operations/incrementactivity.java
+++ Operations/incrementactivity.java
@@ -4,10 +4,10 @@ public class incrementactivity {
     public static void main(String[] args) {
         int x = 12;
         int y = 25;
-        System.out.println("Product of: ");
         System.out.println(x * y);
-
         System.out.println(++x / --y);
+        System.out.println(x++ + y--);
+        System.out.println(y - x);
         
     }
 }   
